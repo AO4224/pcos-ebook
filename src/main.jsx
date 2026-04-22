b8db94b6c93126d8055267a84da10a8479abad2a
Angelo Ortiz <aoxtoo42@MacBookAir.fios-router.home>
2026-04-22 09:47:17 -0400

    fix: import PCOSEbook component instead of missing App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import PCOSEbook from './pcos-ebook.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PCOSEbook />
   </StrictMode>,
 )
