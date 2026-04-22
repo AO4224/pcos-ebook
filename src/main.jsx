2a529d519776f50251eab085c0bef44407c62822
Angelo Ortiz <aoxtoo42@MacBookAir.fios-router.home>
2026-04-22 10:28:03 -0400

    renamed to PCOSFertilityEbook

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import PCOSEbook from './pcos-ebook.jsx'
+import PCOSEbook from './PCOSFertilityEbook.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
